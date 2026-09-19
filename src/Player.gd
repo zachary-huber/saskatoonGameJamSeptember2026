cbf11c1e5c18a34093fdb2ecebad49459da9f0c4
Xander-Boutin <muto2800@outlook.com>
2026-09-19 15:08:48 -0600

    player now has more movement and proper visuals

--- src/Player.gd
+++ src/Player.gd
@@ -2,25 +2,42 @@ class_name Player extends RigidBody2D
 
 var standing : bool = false
 var timer_activated : bool = false
+var setting_direction : bool = false
+var increasing : int = -1
+
+var cur_pos : float = 0
+var last_pos : float = 0
+var velocity : float
 
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var left_detector: RayCast2D = $RayCast2D/LeftDetector
+@onready var right_detector: RayCast2D = $RayCast2D/RightDetector
 @onready var timer: Timer = $Timer
 @onready var chair: Sprite2D = $Chair
 @onready var head_sprite: Sprite2D = $head/HeadSprite
 @onready var right_side: Node2D = $RightSide
 @onready var left_side: Node2D = $LeftSide
+@onready var anchor: Node2D = $Anchor
+@onready var jump_direction_pivot: Marker2D = $Anchor/JumpDirectionPivot
+@onready var jump_direction: Marker2D = $Anchor/JumpDirectionPivot/JumpDirection
 
 func _ready() -> void:
 	left_side.visible = false
 
 func _input(event: InputEvent) -> void:
+	if setting_direction:
+		print(jump_direction.global_position)
+		
+	elif event.is_action_pressed("set direction") and standing:
+		print("setting direction")
+		setting_direction = true
+	
 	if event.is_action_pressed("jump") and standing:
-		print("starting Timer")
 		timer_activated = true
-		timer.start(3)
+		timer.start(2)
 	
 	if event.is_action_released("jump") and timer_activated:
-		var time_left = -timer.time_left + 3
+		var time_left = -timer.time_left + 2
 		var changer : int
 		if right_side.visible == true:
 			changer = 1
@@ -34,27 +51,53 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("stand up"):
 		if standing:
 			disable_standing()
-		elif ray_cast_2d.is_colliding():
+		elif ray_cast_2d.is_colliding() and (velocity < 1.0 and velocity > -1.0):
 			enable_standing()
-	
+
+
 func _physics_process(delta: float) -> void:
 	var direction : float = Input.get_axis("move left", "move right")
 	
 	if standing:
 		if direction < 0:
 			flip_left()
+			if left_detector.is_colliding():
+				print("should stop")
+			else:
+				print("moving left")
+				apply_movement(direction)
 		elif direction > 0:
 			flip_right()
-		global_position = lerp(global_position,ray_cast_2d.get_collision_point() + Vector2(direction * 10, -10), 0.1)
+			if right_detector.is_colliding():
+				print("should stop")
+			else:
+				print("moving right")
+				apply_movement(direction)
+				
+				
 		rotation = lerp(rotation, 0.0, 0.1)
 		if ray_cast_2d.is_colliding() == false:
 			disable_standing()
 	else:
 		find_velocity()
+		constant_torque = direction * 2000
+	
+	if setting_direction:
+		if jump_direction_pivot.rotation_degrees > 0:
+			increasing = -1
+		elif jump_direction_pivot.rotation_degrees < -90:
+			increasing = 1
+		jump_direction_pivot.rotation_degrees += increasing
 	ray_cast_2d.rotation = -rotation
+	anchor.rotation = -rotation
+
+func apply_movement(direction : int) -> void:
+	global_position = lerp(global_position,ray_cast_2d.get_collision_point() + Vector2(direction * 10, -16), 0.1)
 
 func find_velocity() -> void:
-	pass
+	last_pos = cur_pos
+	cur_pos = global_position.x
+	velocity = last_pos - cur_pos
 
 func disable_standing() -> void:
 	standing = false
@@ -65,13 +108,15 @@ func enable_standing() -> void:
 	freeze = true
 
 func flip_left() -> void:
-	chair.flip_h = true
-	head_sprite.flip_h = true
-	right_side.visible = false
-	left_side.visible = true
-
-func flip_right() -> void:
 	chair.flip_h = false
 	head_sprite.flip_h = false
+	right_side.visible = false
+	left_side.visible = true
+	jump_direction.scale.x = -1
+
+func flip_right() -> void:
+	chair.flip_h = true
+	head_sprite.flip_h = true
 	right_side.visible = true
 	left_side.visible = false
+	jump_direction.scale.x = 1
