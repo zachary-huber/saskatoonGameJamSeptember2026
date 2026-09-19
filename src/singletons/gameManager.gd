362db2f27b4696903022950770a537049373969f
zachary-huber <zachuber.zh@gmail.com>
2026-09-18 21:15:28 -0600

    updated game manager to handle run start and end time

--- src/singletons/gameManager.gd
+++ src/singletons/gameManager.gd
@@ -3,16 +3,24 @@ extends Node
 
 var runStartTimeTicks:int = 0
 var runCurrentTimeTicks:int = 0
+var runEndTimeTicks:int = 0
+var currentRunStats:RunStats
 
 func _ready() -> void:
 	pass
 
 func startRun() -> void:
 	setRunStartTimeTicks()
+	resetRunStats()
 
 func endRun() -> void:
-	# return runStats
-	pass
+	setRunEndTimeTicks()
+
+func resetRunStats() -> void:
+	currentRunStats = RunStats.new()
 
 func setRunStartTimeTicks() -> void:
 	runCurrentTimeTicks = Time.get_ticks_usec()
+
+func setRunEndTimeTicks() -> void:
+	runEndTimeTicks = Time.get_ticks_usec()
