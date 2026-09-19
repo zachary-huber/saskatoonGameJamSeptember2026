e021a15638c3a04fe669b12e7de56bb46cb1e339
zachary-huber <zachuber.zh@gmail.com>
2026-09-18 21:45:23 -0600

    added logic to start button, added game scene

--- src/UI/main_menu.gd
+++ src/UI/main_menu.gd
@@ -2,7 +2,7 @@ class_name MainMenu extends Control
 
 
 func _on_start_run_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_quit_button_pressed() -> void:
--- src/singletons/gameManager.gd
+++ src/singletons/gameManager.gd
@@ -2,7 +2,7 @@
 extends Node
 
 var runStartTimeTicks:int = 0
-var runCurrentTimeTicks:int = 0
+var runCurrentTimeSeconds:int = 0
 var runEndTimeTicks:int = 0
 var currentRunStats:RunStats
 
@@ -20,7 +20,7 @@ func resetRunStats() -> void:
 	currentRunStats = RunStats.new()
 
 func setRunStartTimeTicks() -> void:
-	runCurrentTimeTicks = Time.get_ticks_usec()
+	runStartTimeTicks = Time.get_ticks_usec()
 
 func setRunEndTimeTicks() -> void:
 	runEndTimeTicks = Time.get_ticks_usec()
